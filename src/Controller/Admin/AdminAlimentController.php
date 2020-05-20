1c200a3b83fb27b8b06a35d4ad89513e6eb58305
klopfstein <dimitri.kft@gmail.com>
2020-05-20 16:27:14 +0200

    add update for aliments

--- src/Controller/Admin/AdminAlimentController.php
+++ src/Controller/Admin/AdminAlimentController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Aliment;
+use App\Form\AlimentType;
 use App\Repository\AlimentRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,6 +18,20 @@ class AdminAlimentController extends AbstractController
         $aliments = $repository->findAll();
         return $this->render('admin/admin_aliment/adminAliment.html.twig', [
             "aliments" => $aliments
+        ]);
+    }
+
+    /**
+     * @Route("/admin/aliment/{id}", name="admin_aliment_modification")
+     */
+    public function modicication(Aliment $aliment)
+    {
+        $form = $this->createForm(AlimentType::class, $aliment);
+  
+     
+        return $this->render('admin/admin_aliment/modificationAliment.html.twig', [
+            "aliment" => $aliment,
+            "form"    => $form->createView()
         ]);
     }
 }
