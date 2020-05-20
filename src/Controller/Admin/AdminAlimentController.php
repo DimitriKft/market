198735ec7dd88d74c1dc0bd9be6ce40aa5bff2c1
klopfstein <dimitri.kft@gmail.com>
2020-05-20 14:57:38 +0200

    add admin view and init crud for aliments

---
+++ src/Controller/Admin/AdminAlimentController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Repository\AlimentRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminAlimentController extends AbstractController
+{
+    /**
+     * @Route("/admin/aliment", name="admin_aliment")
+     */
+    public function index(AlimentRepository $repository)
+    {
+        $aliments = $repository->findAll();
+        return $this->render('admin/admin_aliment/adminAliment.html.twig', [
+            "aliments" => $aliments
+        ]);
+    }
+}
