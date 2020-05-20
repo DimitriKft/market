b97b1a4fdf57f6c4de3bfc8b8b802b8663e0d946
klopfstein <dimitri.kft@gmail.com>
2020-05-20 10:31:39 +0200

    add list for aliment < 50 calories

--- src/Controller/AlimentController.php
+++ src/Controller/AlimentController.php
@@ -3,19 +3,32 @@
 namespace App\Controller;
 
 use App\Repository\AlimentRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AlimentController extends AbstractController
 {
     /**
      * @Route("/", name="aliments")
-     */
+    */
     public function index(AlimentRepository $repository)
     {
         $aliments = $repository->findAll();
         return $this->render('aliment/aliments.html.twig', [
-            "aliments" => $aliments
+            'aliments' => $aliments,
+            'isCalorie'=> false
+        ]);
+    }
+
+      /**
+     * @Route("/aliments/{calorie}", name="alimentsParCalorie")
+     */
+    public function alimentsMoinsCaloriques(AlimentRepository $repository, $calorie)
+    {
+        $aliments = $repository->getAlimentParNbCalories($calorie);
+        return $this->render('aliment/aliments.html.twig', [
+            "aliments" => $aliments,
+            "isCalorie"=> true
         ]);
     }
 }
