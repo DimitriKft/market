e5e2a06b2308d4ea3d265d85aa030329b2b4f979
klopfstein <dimitri.kft@gmail.com>
2020-05-22 13:42:07 +0200

    bug controller namspace OK

--- src/Controller/Admin/AdminAlimentController.php
+++ src/Controller/Admin/AdminAlimentController.php
@@ -66,4 +66,6 @@ class AdminAlimentController extends AbstractController
         }
     }
 
+  
+
 }
---
+++ src/Controller/AdminTypeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TypeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminTypeController extends AbstractController
+{
+    /**
+     * @Route("/admin/type", name="admin_types")
+     */
+    public function index(TypeRepository $repository)
+    {
+        $types = $repository->findAll();
+        return $this->render('admin_type/adminType.html.twig', [
+            "types" => $types
+        ]);
+    }
+}
